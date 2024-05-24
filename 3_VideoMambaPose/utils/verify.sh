5275a434989b3a396e246388a6663c1ecab006b3
xinlei55555 <xinlei55555@gmail.com>
2024-05-24 06:06:51 -0700

    debug

---
+++ 3_VideoMambaPose/utils/verify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=real_heatmap_train          # Job name
+#SBATCH --account=def-btaati              # Replace with your account
+#SBATCH --time=00-12:00                   # Time limit (DD-HH:MM)
+#SBATCH --cpus-per-task=1                 # Number of CPU cores per task
+#SBATCH --gres=gpu:1                      # Number of GPUs per node 
+#SBATCH --mem=4000M                    # Total memory per node (less means faster allocation) 
+#SBATCH --output=%x-%j.out                # Standard output and error log (%x = job name, %j = job ID)
+
+module load python/3.10
+module load opencv
+module load gcc
+module load cudacore/.12.2.2
+source /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/mamba_env2/bin/activate               # Activate your virtual environment
+
+python verify_installation.py                    # Command to run your Python script
