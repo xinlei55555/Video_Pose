165957187a3aea6d220807bd999d5ad91e12e201
Xin Lei Lin <linxin67@beluga1.int.ets1.calculquebec.ca>
2024-05-28 00:18:02 -0400

    fixed joint regressor

--- 3_VideoMambaPose/src/inference/predict.py
+++ 3_VideoMambaPose/src/inference/predict.py
@@ -2,8 +2,8 @@ import sys
 import os
 
 # change the system directory
-sys.path.append('/home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/3_VideoMambaPose/src/models')
-from experiments.heatmap.HeatVideoMamba import HeatMapVideoMambaPose
+sys.path.append('/home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/3_VideoMambaPose/src/models/experiments/heatmap')
+from HeatVideoMamba import HeatMapVideoMambaPose
 
 import torch 
 import torch.nn as nn
@@ -12,10 +12,41 @@ import torch.nn as nn
 def load_model(filepath):
     # Create the model
     model = HeatMapVideoMambaPose()
+    print(model)
+    print(type(model.state_dict()))
+    # print(model.state_dict().keys())
     
     # Load the state dictionary from the .pt file
-    model.load_state_dict(torch.load(filepath))
-    
+    # model.load_state_dict(torch.load(filepath))
+    checkpoint = torch.load(filepath, map_location=torch.device('cpu'))
+    print(checkpoint)
+    print(type(checkpoint))
+    # model.load_state_dict(checkpoint['model_state_dict'])
+    # print(checkpoint.keys())
+
+    print('checking whether they have the same keys, ', model.state_dict().keys()==checkpoint.keys())
+    # checking which keys differ
+    x = set(model.state_dict().keys()).intersection(set(checkpoint.keys()))
+    print(x)
+    print('that was the intersection ^')
+
+    # checking the keys that are not common
+    y = set(model.state_dict().keys()).union(set(checkpoint.keys()))
+
+    diff = y.difference(x)
+
+    print(diff, 'differeneeceee') #{'joints.regressor.2.bias', 'joints.regressor.0.bias', 'joints.regressor.2.weight', 'joints.regressor.0.weight'}
+
+    # just print the ones with joints.
+    joint = {val for val in y if ('joint' in val)}
+    print(joint, ' --->>>joint')
+
+    print(joint.intersection(set(model.state_dict().keys())))
+    print(joint.intersection(set(checkpoint.keys()))) # the joints are in the checkpoint, but not in them odel I am loading?
+
+    # THE EERRRO was because in my joint regressor, I had not put the layesr in the initisqueuealization funcgtion.
+    model.load_state_dict(checkpoint) # strict = False makes it so that even though some layer are missing, it will work (although idk why some layesr are missing)
+
     # Set model to evaluation mode
     model.eval()
     
@@ -35,7 +66,9 @@ def get_input_and_label(action, file_name, path='/home/linxin67/scratch/JHMDB'):
 
 if __name__ == "__main__":
     # Define the .pt file path
-    model_path = '/home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/3_VideoMambaPose/src/models/experiments/heatmap/checkpoints/heatmap_22069.0820.pt'
+    # model_path = '/home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/3_VideoMambaPose/src/models/experiments/heatmap/checkpoints/heatmap_22069.0820.pt'
+    # model_path = '/home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/3_VideoMambaPose/src/models/experiments/heatmap/checkpoints/heatmap_27345.4473.pt'
+    model_path='/home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/3_VideoMambaPose/src/models/experiments/heatmap/checkpoints/heatmap_8652135.9131.pt'
     
     # Load the model from the .pt file
     model = load_model(model_path)
