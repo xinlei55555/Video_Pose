fixed normalization, debug mode

3_VideoMambaPose/src/models/experiments/heatmap/HeatVideoMamba.py
@@ -34,10 +34,12 @@ class HeatMapVideoMambaPose(nn.Module):
         self.config = config
 
         # encoder
-        self.mamba = hvm.videomamba_tiny()
+        self.mamba = hvm.videomamba_tiny(patch_size=self.config['patch_size'], embed_dim=self.config['embed_channels'])
 
-        patch_height = self.config['image_tensor_height']
-        patch_width = self.config['image_tensor_width']
+        patch_height = self.config['image_tensor_height'] / \
+            self.config['patch_number']
+        patch_width = self.config['image_tensor_width'] / \
+            self.oonfig['patch_number']
         if config['2d_deconv']:
             # Shape of final tensor torch.Size([64, 17, 112, 112])
             # sizes are image_size/patch_number
@@ -49,8 +51,8 @@ class HeatMapVideoMambaPose(nn.Module):
             # 15 is the number of output joints
 
         # output into joints
-        self.joints = hjr.JointOutput(self.config,
-                                      input_channels=self.config['joint_number'], joint_number=self.config['joint_number'], d=1, h=56, w=56, normalize=self.config['normalized'])  # for the JHMBD database
+        self.joints = hjr.JointOutput(
+            self.config, input_channels=self.config['joint_number'], joint_number=self.config['joint_number'], d=1, h=56, w=56, normalize=self.config['normalized'])  # for the JHMBD database
 
     def forward(self, x):
         if self.config['full_debug']:

3_VideoMambaPose/src/models/experiments/heatmap/HeatmapVideoMamba.py
@@ -199,6 +199,7 @@ class PatchEmbed(nn.Module):
         self.tubelet_size = kernel_size
 
         # so they are using conv3d, because you have 3 channels, 16 by 16! (so three diemsni
+        # the convolution is the size of the patch, and they are doing the convolution within each patch, because stride is also patch size
         self.proj = nn.Conv3d(
             in_chans, embed_dim, 
             kernel_size=(kernel_size, patch_size[0], patch_size[1]),
@@ -215,7 +216,7 @@ class VisionMamba(nn.Module):
     def __init__(
             self, 
             img_size=224, 
-            patch_size=16, 
+            patch_size=4, # changing the patch size from 16 to be 4 
             depth=24, 
             embed_dim=192, 
             channels=3, 
@@ -453,7 +454,7 @@ def load_state_dict(model, state_dict, center=True):
 @register_model
 def videomamba_tiny(pretrained=False, **kwargs):
     model = VisionMamba(
-        patch_size=16, 
+        patch_size=4, 
         embed_dim=192, 
         # depth=24, 
         depth=12, # this is what causes the increase in mempry usage.