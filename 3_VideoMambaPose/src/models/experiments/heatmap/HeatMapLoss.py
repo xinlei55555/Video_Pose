c13ce4ac2c47bb8ff760a7394c5a6efe4430ef6f
XLL <xinlei55555@gmail.com>
2024-06-09 10:55:36 -0400

    bounding box detectin

--- 3_VideoMambaPose/src/models/experiments/heatmap/HeatMapLoss.py
+++ 3_VideoMambaPose/src/models/experiments/heatmap/HeatMapLoss.py
@@ -2,9 +2,10 @@ import torch
 import torch.nn as nn
 
 class PoseEstimationLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
         self.mse_loss = nn.MSELoss()
+        self.config = config
 
     def forward(self, predicted, target):
         """
@@ -17,4 +18,7 @@ class PoseEstimationLoss(nn.Module):
         """
         # !TODO I need to change that, because it's not just MSE, I am taking the mse of a 3D value, idk if mse works.
         loss = self.mse_loss(predicted, target)
+        if self.config['show_predictions']: 
+            print(f'The target values are : ', target)
+            print(f'The predicted values are : ', predicted)
         return loss
