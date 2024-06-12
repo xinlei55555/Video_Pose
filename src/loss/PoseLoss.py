ccb76ef8546e538a7787679cd07f7490d585e909
Xin Lei Lin <xinlei55555@gmail.com>
2024-06-12 10:50:50 -0400

    completed pose loss

--- src/loss/PoseLoss.py
+++ src/loss/PoseLoss.py
@@ -1,12 +1,40 @@
 import torch
 import torch.nn as nn
 
+from einops import rearrange
+
 class PoseEstimationLoss(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.mse_loss = nn.MSELoss()
         self.config = config
-        self.velocity_loss = VelocityLoss(config)
+        self.mse_loss = nn.MSELoss()
+        self.velocity_loss = velocity_loss()
+
+    def velocity_loss(self, predicted, target):
+        """
+        Args:
+            predicted (torch.Tensor): The predicted joint positions or heatmaps.
+            target (torch.Tensor): The ground truth joint positions or heatmaps.
+
+        Returns:
+            torch.Tensor: Computed loss.
+                """
+        # n_frames = predicted.shape[-3]
+        predicted = rearrange(predicted, '(b d) j x -> b d j x',
+                              d=self.config['num_frames'], b=self.config['batch_size'])
+        target = rearrange(target, '(b d) j x -> b d j x',
+                           d=self.config['num_frames'], b=self.config['batch_size'])
+
+        # inputted shape should be (B, Num_frames, Joint_number, 2)
+        # MSE is the squared difference of all the values between each two frames
+        # this gives me the difference between each two frames within a video
+        predicted_differences = self.mse_loss(
+            predicted[..., 1:, :, :], predicted[..., :-1, :, :])
+        target_differences = self.mse_loss(
+            target[..., 1:, :, :], target[..., :-1, :, :])
+
+        # then we want the difference between both
+        return abs(target_differences - predicted_differences)
 
     def forward(self, predicted, target):
         """
@@ -17,24 +45,21 @@ class PoseEstimationLoss(nn.Module):
         Returns:
             torch.Tensor: Computed loss.
         """
-        loss = self.mse_loss(predicted, target)
-        if self.config['show_predictions']: 
+        # since now I am taking in predicted and targets for each frame in a video
+        # inputted shape should be (B, Num_frames, Joint_number, 2)
+        predicted = rearrange(predicted, 'b d j x -> (b d) j x')
+        target = rearrange(target, 'b d j x -> (b d) j x')
+
+        calculated_loss = 0.0
+
+        if 'mse' in self.config['losses']:
+            calculated_loss += self.mse_loss(predicted, target)
+
+        if 'velocity' in self.config['losses']:
+            calculated_loss += self.velocity(predicted, target)
+
+        if self.config['show_predictions']:
             print(f'The target values are : ', target)
             print(f'The predicted values are : ', predicted)
-        return loss
 
-class VelocityLoss(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-
-    def forward(self, predicted, target):
-        """
-        Args:
-            predicted (torch.Tensor): The predicted joint positions or heatmaps.
-            target (torch.Tensor): The ground truth joint positions or heatmaps.
-
-        Returns:
-            torch.Tensor: Computed loss.
-        """
-        pass
-
+        return calculated_loss
