21870221f37e8ee863de199c5c1479382b5c8bd3
xinlei55555 <xinlei55555@gmail.com>
2024-06-17 12:40:17 -0700

    new train_Job_cedar

--- slurm/HMR_decoder/train_job_cedar.sh
+++ slurm/HMR_decoder/train_job_cedar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Latent_space       # Job name
 #SBATCH --account=def-btaati              # Replace with your account
-#SBATCH --time=03-00:00                   # Time limit (DD-HH:MM) ! change this when training.
+#SBATCH --time=04-00:00                   # Time limit (DD-HH:MM) ! change this when training.
 #SBATCH --cpus-per-task=1                 # Number of CPU cores per task ! change this when training.
 #SBATCH --gres=gpu:1                      # Number of GPUs per node 
 #SBATCH --mem-per-gpu=64000M                    # Total memory per node (less means faster allocation) 
@@ -12,9 +12,12 @@
 # module load gcc
 #module load cudacore/.11.8.0
 #source /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/mamba_env2/bin/activate               # Activate your virtual environment
+# module restore mamba_modules125
+# source /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/mamba_env125/bin/activate
+# # export WANDB_MODE=offline
+# wandb offline
+cd /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose
 module restore mamba_modules125
-source /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/mamba_env125/bin/activate
-export WANDB_MODE=offline
-wandb offline
+source mamba_env2/bin/activate
 cd /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/src
-python train.py --config 'latent_HMR/latent_HMR_beluga.yaml'                   # Command to run your Python script
+python train.py --config 'HMR_decoder/HMR_decoder_cedar.yaml'                   # Command to run your Python script
