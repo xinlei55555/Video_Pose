9045ee8c4a181052c214cd06b3317e7b54a6ccff
xinlei55555 <xinlei55555@gmail.com>
2024-06-24 10:44:26 -0700

    finished visualization and fixed error pretraining!

--- src/data_format/CocoVideoLoader.py
+++ src/data_format/CocoVideoLoader.py
@@ -56,7 +56,7 @@ class COCOVideoLoader(Dataset):
         # video = rearrange(video, 'd c h w->c d h w')
 
         #-----------------------------------------
-        image, joint, bbox = self.image_data[idx]
+        image, joint, bbox = self.image_data[index]
         # making them all batch size = 1
         # image = image.unsqueeze(0)
         image = rearrange(image, '(d c) h w -> d h w c', d=1)
@@ -65,6 +65,8 @@ class COCOVideoLoader(Dataset):
         #     # some of the bbox have width, and height 0!!!! that means there is nothing in it... (so let me just ignore them in COCOImageLoader)
         image, joint = preprocess_video_data(image.numpy(), bbox.numpy(), joint.numpy(), (self.tensor_width, self.tensor_height), self.min_norm)
         # technically, I have depth = 1... do it's like a one frame video.
+        print(image.shape) # unsure about this step
+        image = rearrange(image, 'd c h w -> c d h w')
         return [image, joint]
         
 
