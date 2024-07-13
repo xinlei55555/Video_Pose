16a565af98a1b32c8c41cf8ab712147c86934172
Xin Lei Lin <xinlei55555@gmail.com>
2024-07-13 15:30:23 -0700

    final

--- slurm/HMR_decoder_coco_pretrain/train_job_cedar.sh
+++ slurm/HMR_decoder_coco_pretrain/train_job_cedar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=HMR_Decoder      # Job name
 #SBATCH --account=def-btaati              # Replace with your account
-#SBATCH --time=05-00:00                   # Time limit (DD-HH:MM) ! change this when training.
+#SBATCH --time=07-00:00                   # Time limit (DD-HH:MM) ! change this when training.
 #SBATCH --cpus-per-task=1                 # Number of CPU cores per task ! change this when training.
 #SBATCH --gres=gpu:1                      # Number of GPUs per node 
 #SBATCH --mem-per-gpu=64000M                    # Total memory per node (less means faster allocation) 
@@ -20,4 +20,4 @@ cd /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose
 module restore mamba_modules125
 source mamba_env2/bin/activate
 cd /home/linxin67/projects/def-btaati/linxin67/Projects/MambaPose/Video_Pose/src
-python train.py --config 'HMR_decoder_coco_pretrain/HMR_decoder_cedar_new_dim.yaml'                   # Command to run your Python script
+python train.py --config 'HMR_decoder_coco_pretrain/fixed_coco_data_HMR_decoder_cedar_new_dim.yaml'                   # Command to run your Python script
